ed79bae8c139673360d8b50ea782a9f2821a079d
Mohamed Ibrahim <maradona.0000@yahoo.com>
2022-06-02 20:31:14 +0200

    New Components

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -17,7 +17,6 @@ axiosClient.interceptors.response.use((response) => {
     if (response && response.data) {
         return response.data;
     }
-
     return response;
 }, (error) => {
     throw error
--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+
+import { Route, Switch } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Catalog from '../pages/Catalog';
+import Detail from '../pages/Detail';
 
 function Routes() {
     return (
-        <div>Routes</div>
+        <Switch>
+            <Route
+                path="/:category/search/:keyword"
+                component={Catalog}
+            />
+            <Route
+                path="/:category/:id"
+                component={Detail}
+            />
+            <Route
+                path="/:category"
+                component={Catalog}
+            />
+            <Route
+                path="/"
+                component={Home}
+            />
+        </Switch>
     )
 }
 
